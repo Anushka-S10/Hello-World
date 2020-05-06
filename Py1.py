7c30dd121363f4abc498cfb2a6c289829d1ce67a
Anushka-S10 <61749789+Anushka-S10@users.noreply.github.com>
2020-05-06 12:30:38 +0530

    Initial

---
+++ Py1.py
@@ -0,0 +1,42 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:19:08) [MSC v.1500 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> msg = "Hello Python"
+>>> print(msg)
+Hello Python
+>>> msg = "Hi there."
+>>> print(msg)
+Hi there.
+>>> #strings
+>>> name = "hello there!"
+>>> print(name)
+hello there!
+>>> print(name.title())
+Hello There!
+>>> print(name.upper())
+HELLO THERE!
+>>> first = "Hello"
+>>> last = "there"
+>>> full = first + " " + last + "!"
+>>> print(full)
+Hello there!
+>>> msg = "However difficult life may seem, there is always something you can do and succeed at."
+>>> famous_person = "Stephen Hawking"
+>>> print(famous_person + " once said,\n" + msg)
+Stephen Hawking once said,
+However difficult life may seem, there is always something you can do and succeed at.
+>>> 
+>>> #Numbers
+>>> print(80+20)
+100
+>>> print(80-20)
+60
+>>> print(80*20)
+1600
+>>> print(80/20)
+4
+>>> num = 5
+>>> print("My lucky number is: " + str(num))
+My lucky number is: 5
+>>> 
