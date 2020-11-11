dd931bfa842b91d282a2a2185eb94b39730188d4
Anushka-S10 <libra.anu500@gmail.com>
2020-11-11 22:13:37 +0530

    updated

---
+++ lists.py
@@ -0,0 +1,16 @@
+#Lists
+guests= ['Percy','Nalini','Sneha','Swati']
+del guests[2] #deletion
+guests.append('Snigdha') #insertion
+print(guests)
+print(len(guests)) #for length
+guests.insert(1,'Aish')
+print(guests)
+popped_guest = guests.pop() #removal of guests
+popped_guest = guests.pop()
+print(guests)
+print(sorted(guests)) # in sorted order
+print(guests[-1])
+for guest in guests:
+	print(guest.title()+", you are invited to attend the party.\n")
+print("Thanks for coming.")
